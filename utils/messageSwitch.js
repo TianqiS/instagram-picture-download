9f28f538833a9a578e37c30aed219812c2b660f6
octopuscat <1322023401@qq.com>
2020-01-23 21:30:39 +0800

    [messageSwitch] fix video upload bug#2

--- utils/messageSwitch.js
+++ utils/messageSwitch.js
@@ -4,31 +4,43 @@ const xmlTool = require('../utils/xmlTool')
 module.exports = async (xmlContent, accessToken) => {
     const MsgType = xmlContent.MsgType[0]
     const Content = xmlContent.Content[0]
+    let returnXml = {}
     let returnBody = ''
     switch (MsgType) {
         case 'text':
             let patt = new RegExp('.*www.instagram.com.*')
             if(patt.test(Content)) {
-                let wechatReturnInfo = await insDownload.downloadPic(Content, accessToken)
-                let mediaId = JSON.parse(wechatReturnInfo.mediaInfo)['media_id']
-                returnBody = xmlTool.jsonToXml({ xml: {
-                        ToUserName: xmlContent.FromUserName,
-                        FromUserName: xmlContent.ToUserName,
-                        CreateTime: Date.now(),
-                        MsgType: wechatReturnInfo.type,
-                        Image: {
-                            MediaId: mediaId
-                        }
-                    }})
-                break
-            }
-            returnBody = xmlTool.jsonToXml({ xml: {
+                const wechatReturnInfo = await insDownload.downloadPic(Content, accessToken)
+                const mediaId = JSON.parse(wechatReturnInfo.mediaInfo)['media_id']
+                const mediaType = wechatReturnInfo.mediaType
+                returnXml = {
                     ToUserName: xmlContent.FromUserName,
                     FromUserName: xmlContent.ToUserName,
                     CreateTime: Date.now(),
-                    MsgType: 'text',
-                    Content: '我已经收到你的消息了哦，我(buhui)会积极回复你的消息的'
-                }})
+                    MsgType: mediaType
+                }
+                switch (mediaType) {
+                    case 'image':
+                        returnXml.Image = {
+                            MediaId: mediaId
+                        }
+                        break
+                    case 'video':
+                        returnXml.Video = {
+                            MediaId: mediaId
+                        }
+                }
+                returnBody = xmlTool.jsonToXml({ xml: returnXml})
+                break
+            }
+            returnXml = {
+                ToUserName: xmlContent.FromUserName,
+                FromUserName: xmlContent.ToUserName,
+                CreateTime: Date.now(),
+                MsgType: 'text',
+                Content: '我已经收到你的消息了哦，我(buhui)会积极回复你的消息的'
+            }
+            returnBody = xmlTool.jsonToXml({ xml: returnXml})
             break
         default:
             returnBody = 'success'
